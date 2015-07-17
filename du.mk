144fbd4b530c2070db2688a6d27156d8d48751be
nobe1976 <charlesmc76@gmail.com>
2015-07-16 21:43:58 -0400

    Setup for DU

---
+++ du.mk
@@ -0,0 +1,32 @@
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/du/config/nfc_enhanced.mk)
+
+# Copy product specific init script to mount SD card
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/init.g2.sdcard.rc:root/init.g2.sdcard.rc
+
+# Inherit device configuration
+$(call inherit-product, device/lge/f320/device.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := f320
+PRODUCT_NAME := du_f320
+PRODUCT_BRAND := LGE
+PRODUCT_MODEL := LG-F320
+PRODUCT_MANUFACTURER := lge
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	BUILD_FINGERPRINT=lge/g2_skt_kr/g2:5.0.1/LRX21Y/F320S28d.1418740902:user/release-keys \
+	PRIVATE_BUILD_DESC="g2_skt_kr-user 5.0.1 LRX21Y F320S28d.1418740902 release-keys"
+
+##PRODUCT_PACKAGES += Torch
