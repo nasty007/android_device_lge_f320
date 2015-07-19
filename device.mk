3bf9eb94b95516c033c13b9b09f92520c8a47fea
nobe1976 <charlesmc76@gmail.com>
2015-07-18 21:05:06 -0400

    Leave This in the device since it is all by itself

--- device.mk
+++ device.mk
@@ -20,6 +20,10 @@ $(call inherit-product, device/lge/g2-common/g2.mk)
 ## overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+-# Init files not shared with G2-common
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init.g2.sdcard.rc:root/init.g2.sdcard.rc
+
 # GSM permissions
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
