17fd325822bd9f939550902f000fc80d571bf08d
intervigilium <intervigil@gmail.com>
2015-11-06 14:09:44 -0500

    f320: Move GSM permission to common

--- device.mk
+++ device.mk
@@ -26,7 +26,6 @@ PRODUCT_COPY_FILES += \
 
 # GSM permissions
 PRODUCT_COPY_FILES += \
-	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
 	frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml
 
 # NFC packages
